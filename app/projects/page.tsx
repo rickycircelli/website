37d549ac0d3c2d3ca6979437a969e904fde5798a
rickycircelli <rickycircelli8@gmail.com>
2025-12-25 18:04:50 -0500

    Initial Site

---
+++ app/projects/page.tsx
@@ -0,0 +1,15 @@
+export default function Projects() {
+  return (
+    <main className="min-h-screen p-10 bg-zinc-50">
+      <h1 className="text-3xl font-bold">Projects</h1>
+
+      <div className="mt-6">
+        <h2 className="text-xl font-semibold">Coming Soon</h2>
+      </div>
+
+      <p className="mt-8 underline">
+        <a href="/">Back home</a>
+      </p>
+    </main>
+  );
+}
